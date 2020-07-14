1d35486ecdd982d495512d39f5417788b1920ab4
nima <nimanoori000@gmail.com>
2020-07-14 21:55:49 +0430

    first commit

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\nima\desktop\dev\mahkala\mahkala_first\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
